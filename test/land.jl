3d30cf91773967b4608880523965646ccf35ed8a
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-01-14 15:33:45 +0530

    small land test too

---
+++ test/land.jl
@@ -0,0 +1,15 @@
+using GeoMakie, Shapefile
+
+shp = Shapefile.Table("/Users/anshul/Downloads/ne_110m_land/ne_110m_land.shp") |> Shapefile.shapes |> skipmissing |> collect
+
+points = rec_point(coordinates.(shp))
+
+source = WGS84()
+
+dest = Projection("+proj=fouc")
+
+meshes = GeoMakie.toMesh.(rec_project(source, dest, points))
+
+geoaxis(-180, 180, -90, 90; show_axis = false, crs = (dest = dest,))
+
+mesh!(meshes)
