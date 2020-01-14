f9bf8ef1147e8f619326b997d2d2b72aedb3973d
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-01-14 15:33:18 +0530

    reexport packages

--- src/GeoMakie.jl
+++ src/GeoMakie.jl
@@ -1,19 +1,38 @@
 module GeoMakie
 
+using Reexport
 using GeometryTypes, EarCut, Colors, MakieLayout, AbstractPlotting
 
 using MakieLayout: Formatting
 
-using GeoInterface, Proj4
+@reexport using Colors, AbstractPlotting
+@reexport using GeoInterface, Proj4
 
 import AbstractPlotting: convert_arguments, convert_attribute
 
-
+# fix conflicts
+const AbstractGeometry = GeometryTypes.AbstractGeometry
+const Point = AbstractPlotting.Point
+const attributes = AbstractPlotting.attributes
+const transform! = Proj4.transform!
+const volume = AbstractPlotting.volume
 
 include("conversions.jl")
+include("utils.jl")
 include("recipes/geoaxis.jl")
 
-export geoaxis, WGS84
+
+# export recipes
+
+export geoaxis, geoaxis!
+
+# export projections
+
+export WGS84
+
+# export utilities
+
+export rec_project, rec_point
 
 end # module
 
