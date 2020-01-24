48472acc8a750e32bc8e54ca8a220789f27b4cb3
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-01-24 13:06:26 -0500

    implement mouse hover

--- test/meshgrid2.jl
+++ test/meshgrid2.jl
@@ -26,6 +26,40 @@ scene = poly(tpoints, faces; color = img[:], show_axis = false);
 
 geoaxis!(scene, -180, 180, -90, 90; crs = (src = src, dest = dest,));
 
+visible = node(:visible, false)
+
+poprect = lift(scene.events.mouseposition) do mp
+    FRect((mp .+ 5), 250, 40)
+end
+textpos = lift(scene.events.mouseposition) do mp
+    Vec3f0((mp .+ 5 .+ (250/2, 40 / 2))..., 120)
+end
+popup = poly!(campixel(scene), poprect, raw = true, color = :white, strokewidth = 2,
+        strokecolor = :black, visible = visible)
+
+rect = popup[end]
+
+translate!(rect, Vec3f0(0, 0, 100))
+
+textnode = Node(" ")
+
+text!(popup, textnode, textsize = 30, position = textpos, color = :darkred, align = (:center, :center), raw = true, visible = visible)
+
+text_field = popup[end]
+
+x = Node(false)
+
+on(scene.events.mouseposition) do pos
+    if ispressed(scene, Mouse.left)
+        visible[] = true
+        pos = to_world(scene, Point2f0(scene.events.mouseposition[]))
+        tpos = transform(dest, source, pos)
+        textnode[] = sprint(io-> print(io, round.(Float64.(tpos), digits = 3)))
+    else
+        visible[] = false
+    end
+end
+
 fullsc = title(scene, titletext)
 
 save("a.png", scene); lines(rand(10))
