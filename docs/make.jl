be0e33284c3347110acdc7a5a40b919c7bbf6b6a
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-01-24 22:04:55 -0500

    general cleanup

--- docs/make.jl
+++ docs/make.jl
@@ -6,13 +6,13 @@ makedocs(;
     pages=[
         "Home" => "index.md",
     ],
-    repo="https://github.com/asinghvi17/GeoMakie.jl/blob/{commit}{path}#L{line}",
+    repo="https://github.com/JuliaPlots/GeoMakie.jl/blob/{commit}{path}#L{line}",
     sitename="GeoMakie.jl",
     authors="Anshul Singhvi",
     assets=String[],
 )
 
 deploydocs(;
-    repo="github.com/asinghvi17/GeoMakie.jl",
+    repo="github.com/JuliaPlots/GeoMakie.jl",
     push_preview = true
 )
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,36 @@
-using GeoMakie
+using GeoMakie, CairoMakie, AbstractPlotting, ImageMagick, NASAEarthObservations
 using Test
 
-@testset "GeoMakie.jl" begin
-    # Write your own tests here.
-end
 
-state_data = download("https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json")
+# download data - ~140 MB
+imgdir = observations("geotiff/CERES_NETFLUX_M")
+
+date_regex(dirname, ext) = Regex("$(dirname)_(\\d{4})-(\\d{2}).$(uppercase(ext))")
+
+img = load(joinpath(imgdir, "CERES_NETFLUX_M_2006-07.TIFF"))
+
+lons = LinRange(-89, 90, size(img)[1])
+lats = LinRange(-179, 180, size(img)[2])
+
+source = LonLat()
+dest = Projection("+proj=robin")
+
+points = GeoMakie.transform.(LonLat(), dest, GeoMakie.gridpoints(lats, lons))
+faces  = GeoMakie.grid_triangle_faces(lats, lons)
+
+
+scene = poly(points, faces; color = imflip(img), show_axis = false);
+
+geoaxis!(scene, -180, 180, -90, 90; crs = (dest = dest,));
+
+titletext = Node("07/2016")
+
+fullsc = title(sc, titletext; fontsize = 40);
+
+record(fullsc, "ceres_netflux.mp4", filter!(x -> uppercase(splitext(x)[2]) == ".TIFF", sort(readdir(imgdir))); framerate = 10) do img
+
+    year, month = match(re, img).captures
+
+    scene.plots[1].color = imflip(ImageMagick.load(joinpath(imgdir,img)))
+    titletext[] = "$month/$year"
+end
