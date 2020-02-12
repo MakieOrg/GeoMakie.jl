277827f2b18e14a611d98144422eb29f61498104
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-02-12 08:24:55 -0500

    Use MakieGallery for testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,39 +1,39 @@
-using GeoMakie, CairoMakie, AbstractPlotting, ImageMagick, NASAEarthObservations, Glob
+using GeoMakie, CairoMakie, MakieGallery, Pkg
 using Test
 
-source = LonLat()
-dest = WinkelTripel()
-
-imgdir = observations("rgb/MYDAL2_M_AER_RA")
-
-imgpaths = sort(Glob.glob(joinpath(relpath(imgdir), "*.PNG"))) # change this to your requirements
-
-img = ImageMagick.load(imgpaths[1])
-
-re = GeoMakie.date_regex("MYDAL2_M_AER_RA", "PNG")
-titletext = Node(join(match(re, basename(imgpaths[1])).captures, '-'))
-
-lons = LinRange(-179.5, 179.5, size(img)[2])
-lats = LinRange(89.5, -89.5, size(img)[1])
-
-xs = [lon for lat in lats, lon in lons]
-ys = [lat for lat in lats, lon in lons]
-
-points, faces = GeoMakie.triangulated_grid(lons, lats)
-tpoints = Proj4.transform.(source, dest, points)
-
-scene = mesh(tpoints, faces; color = img[:], shading = false, show_axis = false, scale_plot = false)
-
-geoaxis!(scene, -180, 180, -90, 90; crs = (src = source, dest = dest,));
-
-titletext = Node("07/2016")
-
-fullsc = title(scene, titletext; fontsize = 40);
-
-record(fullsc, "particulates.mp4", filter!(x -> uppercase(splitext(x)[2]) == ".TIFF", sort(readdir(imgdir))); framerate = 10) do img
-
-    year, month = match(re, img).captures
-
-    scene.plots[1].color = ImageMagick.load(joinpath(imgdir,img))
-    titletext[] = "$month/$year"
+if success(`glxinfo`) && GLMakie in keys(Pkg.project().dependencies)
+    using GLMakie
+    GLMakie.activate!()
 end
+
+# Download reference images from master
+MakieGallery.current_ref_version[] = "master"
+
+
+MakieGallery.load_database(["geomakie.jl"]);
+
+filter!(MakieGallery.database) do entry
+    entry.name !∈ ("Air particulates")
+end
+
+tested_diff_path = joinpath(@__DIR__, "tested_different")
+test_record_path = joinpath(@__DIR__, "test_recordings")
+
+isdir(tested_diff_path) && rm(tested_diff_path; force = true, recursive = true)
+mkpath(tested_diff_path)
+
+isdir(test_record_path) && rm(test_record_path; force = true, recursive = true)
+mkpath(test_record_path)
+
+examples = MakieGallery.record_examples(test_record_path)
+
+@test length(examples) == length(database)
+
+# MakieGallery.generate_preview(test_record_path, joinpath(homedir(), "Desktop", "index.html"))
+# MakieGallery.generate_thumbnails(test_record_path)
+# MakieGallery.gallery_from_recordings(test_record_path, joinpath(test_record_path, "index.html"))
+
+printstyled("Running ", color = :green, bold = true)
+println("visual regression tests")
+
+MakieGallery.run_comparison(test_record_path, tested_diff_path)
