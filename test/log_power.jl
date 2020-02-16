617fd535a4e0b6ea77b8fabb565cf537182acca0
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-02-16 14:18:39 -0500

    refactor to ExpTransformation

--- test/log_power.jl
+++ test/log_power.jl
@@ -1,26 +1,32 @@
+# # Implementing log/exponent transformations
+# The CoordinateTransformations library provides a flexible and extensible framework
+# for constructing coordinate transformations.
+
 using CoordinateTransformations
 
 struct LogTransformation{T <: Real} <: CoordinateTransformations.Transformation
     base
 end
 
-struct PowerTransformation{T <: Real} <: CoordinateTransformations.Transformation
+struct ExponentialTransformation{T <: Real} <: CoordinateTransformations.Transformation
     base::T
 end
+
+const ExpTransformation = ExponentialTransformation
 
 function LogTransformation(; base::T = ℯ) where T
     return LogTransformation(base)
 end
 
-function PowerTransformation(; base::T = ℯ) where T
-    return PowerTransformation(base)
+function ExponentialTransformation(; base::T = ℯ) where T
+    return ExponentialTransformation(base)
 end
 
 (lt::LogTransformation)(x) = log.(lt.base, x)
-(pt::PowerTransformation)(x) = lt.base .^ x
+(pt::ExponentialTransformation)(x) = lt.base .^ x
 
-Base.inv(lt::LogTransformation) = PowerTransformation(lt.base)
-Base.inv(pt::PowerTransformation) = LogTransformation(pt.base)
+Base.inv(lt::LogTransformation) = ExponentialTransformation(lt.base)
+Base.inv(pt::ExponentialTransformation) = LogTransformation(pt.base)
 
-compose(lt::LogTransformation, pt::PowerTransformation) = lt.base == pt.base ? IdentityTransformation() : ComposedTransformation(lt, pt)
-compose(pt::PowerTransformationlt::LogTransformation) = lt.base == pt.base ? IdentityTransformation() : ComposedTransformation(pt, lt)
+compose(lt::LogTransformation, pt::ExponentialTransformation) = lt.base == pt.base ? IdentityTransformation() : ComposedTransformation(lt, pt)
+compose(pt::ExponentialTransformationlt::LogTransformation) = lt.base == pt.base ? IdentityTransformation() : ComposedTransformation(pt, lt)
