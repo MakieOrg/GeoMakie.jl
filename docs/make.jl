1cfd6b21bd3aa91906bb3d2e5e77ab531a5aef6e
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-02-21 16:53:54 -0500

    allow local doc deployment

--- docs/make.jl
+++ docs/make.jl
@@ -19,7 +19,33 @@ makedocs(;
     authors="Anshul Singhvi",
 )
 
+
+struct Local <: Documenter.DeployConfig end
+
+function Documenter.deploy_folder(cfg::Local;
+        repo, devbranch, push_preview, devurl, kwargs...)
+
+        devurl
+
+    # folder = if get(ENV, "PUSH_LOCAL_BUILD", "false") == "true"
+    #     @warn("Setting ENV[\"PUSH_LOCAL_BUILD\"] = \"false\", remember to set it to true for the next push.")
+    #     ENV["PUSH_LOCAL_BUILD"] = "false"
+    #     devurl
+    # else
+    #     @warn("Set ENV[\"PUSH_LOCAL_BUILD\"] = \"true\" if you want your local build to be pushed to Github Pages.")
+    #     nothing
+    # end
+end
+
+Documenter.authentication_method(::Local) = Documenter.SSH
+
+function Documenter.documenter_key(::Local)
+    open(readchomp, expanduser("~/documenter.geomakie.key")) * '\n'
+end
+
+Documenter.documenter_key(Local()) |> length
 deploydocs(;
     repo="github.com/JuliaPlots/GeoMakie.jl",
-    push_preview = true
+    push_preview = true,
+    # deploy_config = Local()
 )
