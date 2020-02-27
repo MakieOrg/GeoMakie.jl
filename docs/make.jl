85adbc086baf64e91a1e3b289ec37f28261aaa49
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-02-27 09:01:39 -0500

    index html

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,13 @@
 using Documenter, GeoMakie
 
 # use the README as the home page
-cp(joinpath(dirname(@__DIR__), "README.md"), joinpath(@__DIR__, "src", "README.md"); force = true)
+cp(joinpath(dirname(@__DIR__), "README.md"), joinpath(@__DIR__, "src", "index.md"); force = true)
 
 makedocs(;
     modules=[GeoMakie, AbstractPlotting],
     format=Documenter.HTML(),
     pages=[
-        "Home" => "README.md",
+        "Home" => "index.md",
         "Recipes" => [
             "geoaxis.md",
             "stock.md"
