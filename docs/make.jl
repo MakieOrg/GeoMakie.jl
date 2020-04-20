d5617573d2e0e761cbc877ffee694270919f72f2
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-04-20 11:59:35 +0530

    cache artifacts

--- docs/make.jl
+++ docs/make.jl
@@ -14,7 +14,7 @@ makedocs(;
     format=Documenter.HTML(),
     pages=[
         "Home" => "index.md",
-        "Example" => "example.md"
+        "Example" => "example.md",
         "Recipes" => [
             "geoaxis.md",
             "stock.md"
@@ -25,31 +25,6 @@ makedocs(;
     sitename="GeoMakie.jl",
     authors="Anshul Singhvi",
 )
-
-
-#struct Local <: Documenter.DeployConfig end
-
-#function Documenter.deploy_folder(cfg::Local;
-#        repo, devbranch, push_preview, devurl, kwargs...)
-#
-#        devurl
-#
-    # folder = if get(ENV, "PUSH_LOCAL_BUILD", "false") == "true"
-    #     @warn("Setting ENV[\"PUSH_LOCAL_BUILD\"] = \"false\", remember to set it to true for the next push.")
-    #     ENV["PUSH_LOCAL_BUILD"] = "false"
-    #     devurl
-    # else
-    #     @warn("Set ENV[\"PUSH_LOCAL_BUILD\"] = \"true\" if you want your local build to be pushed to Github Pages.")
-    #     nothing
-    # end
-#end
-
-#Documenter.authentication_method(::Local) = Documenter.SSH
-
-#function Documenter.documenter_key(::Local)
-#    open(readchomp, expanduser("~/documenter.geomakie.key")) * '\n'
-#end
-
 
 deploydocs(;
     repo="github.com/JuliaPlots/GeoMakie.jl",
