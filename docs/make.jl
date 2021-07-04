007571fe4133ca75cdab7047b4220dcabccb1374
Martijn Visser <mgvisser@gmail.com>
2021-07-04 11:09:39 +0200

    begin fixing the docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,27 +1,23 @@
 using Documenter, GeoMakie
 
 # use the README as the home page
-cp(joinpath(dirname(@__DIR__), "README.md"), joinpath(@__DIR__, "src", "index.md"); force = true)
-
-indexstr = read(joinpath(@__DIR__, "src", "index.md"), String)
-
-replace(indexstr, """<a href = "https://www.github.com/JuliaPlots/Makie.jl"><img src="https://raw.githubusercontent.com/JuliaPlots/Makie.jl/master/assets/logo.png" alt="Makie.jl" height="30" align = "top"></a>""" => "Makie.jl")
-
-write(joinpath(@__DIR__, "src", "index.md"), indexstr)
+readme = read(joinpath(dirname(@__DIR__), "README.md"), String)
+# HTML not supported in the Markdown parser used by Documenter
+href = """<a href = "https://www.github.com/JuliaPlots/Makie.jl"><img src="https://raw.githubusercontent.com/JuliaPlots/Makie.jl/master/assets/logo.png" alt="Makie.jl" height="30" align = "top"></a>"""
+homepage = replace(readme, href => "Makie.jl")
+write(joinpath(@__DIR__, "src", "index.md"), homepage)
 
 makedocs(;
-    modules=[GeoMakie, Makie],
+    modules=[GeoMakie],
     format=Documenter.HTML(),
     pages=[
         "Home" => "index.md",
         "Example" => "example.md",
         "Recipes" => [
-            "geoaxis.md",
             "stock.md"
         ],
         "API Reference" => "api.md"
     ],
-    repo="https://github.com/JuliaPlots/GeoMakie.jl/blob/{commit}{path}#L{line}",
     sitename="GeoMakie.jl",
     authors="Anshul Singhvi",
 )
@@ -29,5 +25,4 @@ makedocs(;
 deploydocs(;
     repo="github.com/JuliaPlots/GeoMakie.jl",
     push_preview = true,
-    # deploy_config = Local()
 )
