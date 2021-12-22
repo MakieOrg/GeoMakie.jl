4b081feeaeb94ccc0fdb04a083253d50fa8b65b5
SimonDanisch <sdanisch@gmail.com>
2021-12-22 14:48:50 +0100

    clean up make

--- docs/make.jl
+++ docs/make.jl
@@ -1,28 +1,17 @@
-cd(@__DIR__)
-using Pkg
-CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
-Pkg.activate(@__DIR__)
-CI && Pkg.instantiate()
-
 using Documenter, GeoMakie
+
+deploy = length(ARGS) == 1 && ARGS[1] == "deploy"
+if deploy && !haskey(ENV, "GITHUB_TOKEN")
+    @warn("Not deploying, no GITHUB_TOKEN not found in ENV")
+    deploy = false
+end
 
 makedocs(;
     modules=[GeoMakie],
-    doctest = false,
-    format = Documenter.HTML(
-        prettyurls = CI, collapselevel = 3,
-    ),
-    pages=[
-        "GeoMakie.jl" => "index.md",
-    ],
+    doctest=false,
+    format=Documenter.HTML(; prettyurls=deploy, collapselevel=3),
+    pages=["GeoMakie.jl" => "index.md"],
     sitename="GeoMakie.jl",
-    authors = "Makie.jl contributors",
-)
+    authors="Makie.jl contributors")
 
-if CI
-    deploydocs(;
-        repo="github.com/JuliaPlots/GeoMakie.jl",
-        target = "build",
-        push_preview = true,
-    )
-end
+deploy && deploydocs(; repo="github.com/JuliaPlots/GeoMakie.jl", target="build", push_preview=true)
