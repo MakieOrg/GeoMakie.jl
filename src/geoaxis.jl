af3983ebe358936670f7ab2e6d5d3d18fda7b3ea
Anshul Singhvi <anshulsinghvi@gmail.com>
2022-04-24 11:55:09 -0400

    Revamp geoaxis
    Since https://github.com/JuliaPlots/GeoMakie.jl/commit/8bfe471d5dfc82d2eab590a9574811fed62fbd7b, the geoaxis has been built on top of the regular MakieLayout Axis.  This is a great idea and 
    streamlines a lot of the code, but in doing so a lot of the original logic
    was not reimplemented in terms of ticklabels and other things.  Currently, 
    GeoAxis uses the normal Axis ticks, which are placed in screen-space, not dataspace,
    meaning that they are not subject to the nonlinear transform.
    
    This commit hooks into and hijacks some of the Axis machinery in order to allow 
    the standard methods of interacting with an Axis to work with GeoAxis.  As such,
    the tick finding algorithm is now linked in to the Axis; minor grids have still
    not been implemented, but those are easy to implement with the current foundation.  
    Visibility, color, linestyle, linewidth, etc. are all forward from the usual Axis
    attributes, and the Axis default drawings are all set to be invisible.  
    
    As of now, tick labels are close to working; there is an odd scaling factor which
    I have yet to figure out.  However, after that is done, GeoMakie's functionality
    should be back to (and better than) the original!
    
    This means that zooming should now work smoothly, though I have not been able to
    test it yet.  I am currently working on trying to get the labels to work; the
    next feature after that will be automatic limit finding with the algorithm from 
    https://github.com/JuliaPlots/GeoMakie.jl/pull/100.

--- src/geoaxis.jl
+++ src/geoaxis.jl
@@ -1,3 +1,6 @@
+using Makie: left, right, top, bottom
+using Makie.MakieLayout: height, width
+
 """
     GeoAxis(args...; kwargs...) → ax
 Create a new axis instance `ax` that is a modified `Axis` of the Makie.jl ecosystem.
@@ -12,11 +15,11 @@ In the call signature, `args...` is a standard figure location, e.g., `fig[1,1]`
 `Axis`. The keyword arguments decide the geospatial projection:
 
 * `source = "+proj=longlat +datum=WGS84", dest = "+proj=eqearth"`: These two keywords
-  configure the map projection to be used for the given field using Proj4.jl.
+  configure the map projection to be used for the given field using Proj.jl.
   See also online the section [Changing central longitude](@ref) for data that may not
   span the (expected by default) longitude range from -180 to 180.
-* `transformation = Proj4.Transformation(source, dest, always_xy=true)`: Instead of
-  `source, dest` you can directly use the Proj4.jl package to define the projection.
+* `transformation = Proj.Transformation(source, dest, always_xy=true)`: Instead of
+  `source, dest` you can directly use the Proj.jl package to define the projection.
   lines!(ax, GeoMakie.coastlines(); coastkwargs...)
 * `lonticks = -180:60:180, latticks = -90:30:90` ticks for the longitude and latitude
   dimensions. The grid lines of the axis are also spanning these tick values.
@@ -55,74 +58,282 @@ display(fig)
 """
 function GeoAxis(args...;
         source = "+proj=longlat +datum=WGS84", dest = "+proj=eqearth",
-        transformation = Proj4.Transformation(source, dest, always_xy=true),
-        lonticks = -180:60:180,
-        latticks = -90:30:90,
-        hidespines = true,
+        transformation = Proj.Transformation(source, dest, always_xy=true),
+        lonlims = (-180, 180),
+        latlims = (-90, 90),
+        hide_original_spines = true,
         coastlines = false,
         coastline_attributes = (;),
-        aspect = DataAspect(),
+        line_density = 1_000,
+        # these are the axis keywords which we will merge in
+        xtickformat = geoformat_ticklabels,
+        ytickformat = geoformat_ticklabels,
+        xticks = LinearTicks(7),
+        yticks = LinearTicks(7),
         kw...
     )
     ptrans = transformation
 
-    xticks = first.(Makie.apply_transform(ptrans, Point2f0.(lonticks, latticks[1])))
-    yticks = last.(Makie.apply_transform(ptrans, Point2f0.(lonticks[1], latticks)))
+    # TODO: Ensure that the limits are valid
+    # validate_limits(transformation, lonlims, latlims)
+
+    # Apply defaults
     # Generate Axis instance
     ax = Axis(args...;
-        aspect = aspect,
-        xticks = (xticks, string.(lonticks, 'ᵒ')),
-        yticks = (yticks, string.(latticks, 'ᵒ')),
-        limits = (extrema(lonticks), extrema(latticks)),
+        aspect = DataAspect(),
+        xtickformat = xtickformat,
+        ytickformat = ytickformat,
+        xticks = xticks,
+        yticks = yticks,
+        limits = (lonlims, latlims),
         kw...)
+
 
     # Set axis transformation
     ax.scene.transformation.transform_func[] = ptrans
 
     # Plot coastlines
-    if coastlines
-        coastplot = lines!(ax, GeoMakie.coastlines(); color = :black, coastline_attributes...)
-        translate!(coastplot, 0, 0, 99) # ensure they are on top of other plotted elements
-    end
-    # Draw tick lines
-    ax.xgridvisible=false; ax.ygridvisible=false
-    # TODO: How to get "default" grid line style from the theme?
-    for lon in lonticks
-        coords = [Point2f(lon, l) for l in range(latticks[1], latticks[end]; length = 100)]
-        gridplot = lines!(ax, coords; color = :gray20, linewidth = 0.5)
-        translate!(gridplot, 0, 0, 100) # ensure they are on top of other plotted elements
+    coastplot = lines!(ax, GeoMakie.coastlines(); color = :black, coastline_attributes...)
+    translate!(coastplot, 0, 0, 99) # ensure they are on top of other plotted elements
+    xprot = ax.xaxis.protrusion[]
+    yprot = ax.yaxis.protrusion[]
+    if !coastlines
+        delete!(ax, coastplot)
     end
 
-    for lat in latticks
-        coords = [Point2f(l, lat) for l in range(lonticks[1], lonticks[end]; length = 100)]
-        gridplot = lines!(ax, coords; color = :gray20, linewidth = 0.5)
-        translate!(gridplot, 0, 0, 100) # ensure they are on top of other plotted elements
-    end
+    # Set the axis's native grid to always be invisible, and
+    # forward those updates to our observables.
+    # First we need to hijack the axis's protrusions and store them
 
-    hidespines && hidespines!(ax)
+    hijacked_observables = Dict{Symbol, Observable}()
+    ## This macro is defined in `utils.jl`
+    @hijack_observable :xgridvisible
+    @hijack_observable :ygridvisible
+    @hijack_observable :xticksvisible
+    @hijack_observable :yticksvisible
+    @hijack_observable :xticklabelsvisible
+    @hijack_observable :yticklabelsvisible
+    @hijack_observable :topspinevisible
+    @hijack_observable :bottomspinevisible
+    @hijack_observable :leftspinevisible
+    @hijack_observable :rightspinevisible
+
+
+    # WARNING: for now, we only accept xticks on the bottom
+    # and yticks on the left.
+
+    draw_geoticks!(ax, hijacked_observables, line_density)
+
+    ax.xaxis.protrusion[] = xprot
+    ax.yaxis.protrusion[] = yprot
+
     return ax
 end
 
+function draw_geoticks!(ax::Axis, hijacked_observables, line_density)
+    topscene = ax.blockscene
+    scene = ax.scene
 
-function Makie.apply_transform(f::Proj4.Transformation, pt::Point{N,T}) where {N,T}
-    Point(f(Vec(pt)) ./ 100_000)
-end
-function Makie.apply_transform(f::Proj4.Transformation, r::Rect2{T}) where {T}
-    # TODO: once Proj4.jl is updated to PROJ 8.2, we can use
-    # proj_trans_bounds (https://proj.org/development/reference/functions.html#c.proj_trans_bounds)
-    N = 21
-    umin = vmin = T(Inf)
-    umax = vmax = T(-Inf)
-    xmin, ymin = minimum(r)
-    xmax, ymax = maximum(r)
-    for x in range(xmin, xmax; length = N)
-        for y in range(ymin, ymax; length = N)
-            u, v = Makie.apply_transform(f, Point(x, y))
-            umin = min(umin, u)
-            umax = max(umax, u)
-            vmin = min(vmin, v)
-            vmax = max(vmax, v)
+    decorations = Dict{Symbol, Any}()
+
+    xgridpoints = Observable(Point2f[])
+    ygridpoints = Observable(Point2f[])
+
+    xtickpoints = Observable(Point2f[])
+    ytickpoints = Observable(Point2f[])
+
+    xticklabels = Observable(String[])
+    yticklabels = Observable(String[])
+
+    topspinepoints = Observable(Point2f[])
+    btmspinepoints = Observable(Point2f[])
+    lftspinepoints = Observable(Point2f[])
+    rgtspinepoints = Observable(Point2f[])
+
+    xlimits = Observable((0.0f0, 0.0f0))
+    ylimits = Observable((0.0f0, 0.0f0))
+    # First we establish the spine points
+
+    lift(ax.finallimits, ax.xticks, ax.xtickformat, ax.yticks, ax.ytickformat, ax.scene.px_area) do limits, xticks, xtickformat, yticks, ytickformat, pxarea
+
+        lmin = minimum(limits)
+        lmax = maximum(limits)
+        xlimits[] = (lmin[1], lmax[1])
+        ylimits[] = (lmin[2], lmax[2])
+
+        _xtickvalues, _xticklabels = Makie.MakieLayout.get_ticks(xticks, identity, xtickformat, xlimits[]...)
+        _ytickvalues, _yticklabels = Makie.MakieLayout.get_ticks(yticks, identity, ytickformat, ylimits[]...)
+
+        # update but do not notify
+        # project xtickpoints from inputspace to scene dataspace (do transform)
+        # then project to scene pixelspace.
+        # TODO this is still not working right - the text positions almost look stretched
+        # or multiplied out from where they should be.
+        xtickpoints.val = Point2f.(
+            Makie.project.(
+                # obtain the camera of the Scene which will project to its screenspace
+                Ref(camera(scene)),
+                # go from dataspace (transformation applied to inputspace) to pixelspace
+                Ref(:data), Ref(:pixel),
+                # apply the transform to go from inputspace to dataspace
+                Makie.apply_transform(
+                    scene.transformation.transform_func[],
+                    Point2f.(_xtickvalues, ylimits[][1])
+                )
+            )
+        ) .+ Ref(Point2f(pxarea.origin))
+
+        ytickpoints.val = Point2f.(
+            Makie.project.(
+                Ref(camera(scene)),
+                Ref(:data), Ref(:pixel),
+                Makie.apply_transform(
+                    scene.transformation.transform_func[],
+                    Point2f.(xlimits[][1], _ytickvalues)
+                )
+            )
+        ) .+ Ref(Point2f(pxarea.origin))
+
+        # notify this
+        xticklabels[] = _xticklabels
+        yticklabels[] = _yticklabels
+
+        xrange = LinRange(xlimits[]..., line_density)
+        yrange = LinRange(ylimits[]..., line_density)
+
+        # first update the spine
+        topspinepoints[] = Point2f.(xrange, ylimits[][2])
+        btmspinepoints[] = Point2f.(xrange, ylimits[][1])
+        lftspinepoints[] = Point2f.(xlimits[][1], yrange)
+        rgtspinepoints[] = Point2f.(xlimits[][2], yrange)
+
+        # now, the grid.  Each visible "gridline" is separated from the next
+        # by a `Point2f(NaN)`.  The approach here allows us to avoid appending.
+        # x first
+        _xgridpoints = fill(Point2f(NaN), (line_density+1) * length(_xtickvalues))
+
+        current_ind = 1
+        for x in _xtickvalues
+            _xgridpoints[current_ind:(current_ind+line_density-1)] = Point2f.(x, yrange)
+            current_ind += line_density + 1
         end
+        # now y
+        _ygridpoints = fill(Point2f(NaN), (line_density+1) * length(_xtickvalues))
+
+        current_ind = 1
+        for y in _ytickvalues
+            _ygridpoints[current_ind:(current_ind+line_density-1)] = Point2f.(xrange, y)
+            current_ind += line_density + 1
+        end
+
+        xgridpoints[] = _xgridpoints
+        ygridpoints[] = _ygridpoints
+
+        return 1
+        # Now, we've updated the entire axis.
     end
-    Rect(Vec2(umin, vmin), Vec2(umax-umin, vmax-vmin))
+
+    Makie.Observables.notify!(ax.xticks)
+
+    # Time to plot!
+
+    # First, we plot the spines:
+
+    decorations[:topspineplot] = lines!(
+        scene, topspinepoints;
+        visible = hijacked_observables[:topspinevisible],
+        color = ax.topspinecolor,
+        # linestyle = ax.spinestyle,
+        linewidth = ax.spinewidth, inspectable = false
+        )
+    decorations[:btmspineplot] = lines!(
+        scene, btmspinepoints;
+        visible = hijacked_observables[:bottomspinevisible],
+        color = ax.bottomspinecolor,
+        # linestyle = ax.spinestyle,
+        linewidth = ax.spinewidth, inspectable = false
+        )
+    decorations[:lftspineplot] = lines!(
+        scene, lftspinepoints;
+        visible = hijacked_observables[:leftspinevisible],
+        color = ax.leftspinecolor,
+        # linestyle = ax.spinestyle,
+        linewidth = ax.spinewidth,
+        inspectable = false
+        )
+    decorations[:rgtspineplot] = lines!(
+        scene, rgtspinepoints;
+        visible = hijacked_observables[:rightspinevisible],
+        color = ax.rightspinecolor,
+        # linestyle = ax.spinestyle,
+        linewidth = ax.spinewidth,
+        inspectable = false
+        )
+
+
+    # Now for the grids:
+
+    decorations[:xgridplot] = lines!(
+        scene, xgridpoints;
+        visible = hijacked_observables[:xgridvisible],
+        color = ax.xgridcolor,
+        linestyle = ax.xgridstyle,
+        width = ax.xgridwidth,
+        inspectable = false
+    )
+    decorations[:ygridplot] = lines!(
+        scene, ygridpoints;
+        visible = hijacked_observables[:ygridvisible],
+        color = ax.ygridcolor,
+        linestyle = ax.ygridstyle,
+        width = ax.ygridwidth,
+        inspectable = false
+    )
+
+
+    # And finally, the TikZ!
+
+    # ax_scenearea = Makie.MakieLayout.sceneareanode!(ax.layoutobservables.computedbbox, ax.finallimits, ax.aspect)
+    # # # This is specifically a Scene for the text!
+    # textscene = Scene(ax.blockscene, px_area=ax_scenearea)
+
+    textscene = ax.blockscene
+
+    # TODO: fix text!
+    decorations[:xtickplot] = text!(
+        textscene,
+        xticklabels;
+        markerspace = :screen,
+        visible = hijacked_observables[:xticklabelsvisible],
+        position = xtickpoints,
+        rotation = ax.xticklabelrotation,
+        font = ax.xticklabelfont,
+        fontsize = ax.xticklabelsize,
+        color = ax.xticklabelcolor,
+        align = (:center, :top),
+        inspectable = false
+    )
+
+    decorations[:ytickplot] = text!(
+        textscene,
+        yticklabels;
+        markerspace = :screen,
+        visible = hijacked_observables[:yticklabelsvisible],
+        position = ytickpoints,
+        rotation = ax.yticklabelrotation,
+        font = ax.yticklabelfont,
+        fontsize = ax.yticklabelsize,
+        color = ax.yticklabelcolor,
+        align = (:right, :center),
+        inspectable = false
+    )
+
+    scatter!(textscene, xtickpoints; visible = hijacked_observables[:xticklabelsvisible], color = :red, bordercolor=:black)
+    scatter!(textscene, ytickpoints; visible = hijacked_observables[:yticklabelsvisible], color = :red, bordercolor=:black)
+
+    # Finally, we translate these plots such that they are above the content.
+    Makie.update_cam!(scene)
+    translate!.(values(decorations), 0, 0, 100)
+
+    return decorations
 end
