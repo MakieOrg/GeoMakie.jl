baefef7126606765bead11d8af6bfe9057f328ee
Anshul Singhvi <anshulsinghvi@gmail.com>
2022-04-24 11:42:09 -0400

    Create utils.jl
    Added some utility functions and moved the transformation infrastructure over to utils.jl.
    
    New functions are a ticklabel formatter and a macro which will be useful for the Geoaxis refactor.

---
+++ src/utils.jl
@@ -0,0 +1,146 @@
+############################################################
+#                                                          #
+#         Proj.Transform as a Makie transformation         #
+#                                                          #
+############################################################
+
+# This function is a little gnarly.
+# In Makie, we use NaN as a blank point, i.e. any line
+# ends at NaN and restarts at the next point.
+# However, if NaN goes somewhere in ℝ² in the projection,
+# this utility is lost and the plot will have a bunch of lines
+# going to transformation(Point2f(NaN)).
+# In order to avoid this, we check first if the point is NaN,
+# if so we return NaN.  Then, and only then, do we transform it.
+# This does not seem to effect times too much but can be removed
+# if necessary.
+function Makie.apply_transform(t::Proj.Transformation, pt::Point{N,T}) where {N,T}
+    if all(isnan.(pt))
+        return Point{N, T}(NaN)
+    end
+    # this is to catch errors - show the point which was invalid
+    # and then catch it.
+    try
+        f = Point(t(Vec(pt)) ./ 100_000)
+        return f
+    catch e
+        println("Invalid point for transformation!")
+        @show pt
+        rethrow(e)
+    end
+end
+
+
+function Makie.apply_transform(f::Proj.Transformation, r::Rect2{T}) where {T}
+    # TODO: once Proj4.jl is updated to PROJ 8.2, we can use
+    # proj_trans_bounds (https://proj.org/development/reference/functions.html#c.proj_trans_bounds)
+    N = 21
+    umin = vmin = T(Inf)
+    umax = vmax = T(-Inf)
+    xmin, ymin = minimum(r)
+    xmax, ymax = maximum(r)
+    for x in range(xmin, xmax; length = N)
+        for y in range(ymin, ymax; length = N)
+            u, v = Makie.apply_transform(f, Point(x, y))
+            umin = min(umin, u)
+            umax = max(umax, u)
+            vmin = min(vmin, v)
+            vmax = max(vmax, v)
+        end
+    end
+
+    return Rect(Vec2(umin, vmin), Vec2(umax-umin, vmax-vmin))
+end
+
+Makie.inverse_transform(p::Proj.Transformation) = Base.inv(p)
+
+# This is the code for the function body of `apply_transform(f::Proj4.Transformation, r::Rect2)` once Proj4.jl is renamed to Proj.jl
+# out_xmin = Ref{Float64}(0.0)
+# out_ymin = Ref{Float64}(0.0)
+# out_xmax = Ref{Float64}(0.0)
+# out_ymax = Ref{Float64}(0.0)
+# try
+#     Proj.proj_trans_bounds(C_NULL, f.pj, Proj.PJ_FWD, minimum(r)..., maximum(r)..., out_xmin, out_ymin, out_xmax, out_ymax, N)
+# catch e
+#     @show r
+#     @show out_xmin[] out_xmax[] out_ymin[] out_ymax[]
+#     rethrow(e)
+# end
+#
+# return Rect2{T}((out_xmin[], out_xmax[] - out_xmin[]), (out_ymin[], out_ymax[] - out_ymin[]))
+
+
+############################################################
+#                                                          #
+#              Tick and tick label utilities               #
+#                                                          #
+############################################################
+
+"""
+    geoformat_ticklabels(nums::Vector)
+
+A semi-intelligent formatter for geographic tick labels.  Append `"ᵒ"` to the end
+of each tick label, to indicate degree.
+
+This will check whether the ticklabel is an integer value (`round(num) == num`).
+If so, label as an Int (1 instead of 1.0) which looks a lot cleaner.
+
+## Example
+```julia-repl
+julia> geoformat_ticklabels([1.0, 1.1, 2.5, 25])
+4-element Vector{String}:
+ "1ᵒ"
+ "1.1ᵒ"
+ "2.5ᵒ"
+ "25ᵒ"
+ ```
+"""
+function geoformat_ticklabels(nums)
+    labels = fill("", length(nums))
+    for i in 1:length(nums)
+        labels[i] = if round(nums[i]) == nums[i]
+            string(Int(nums[i]), 'ᵒ')
+        else
+            string(nums[i], 'ᵒ')
+        end
+    end
+    return labels
+end
+
+
+############################################################
+#                                                          #
+#                      Useful macros                       #
+#                                                          #
+############################################################
+
+"""
+    @hijack_observable name::Symbol
+
+Assuming the presence of a `hijacked_observables::Dict{Symbol, Any}` and `ax::Axis`,
+hijacks the Observable `ax[name]` and redirects all its updates to `hijacked_observables[name]`,
+while keeping `ax[name]` as `false`.
+
+More technically, this macro injects a function at the beginning of `ax[name].listeners`, which
+forwards whatever update was made to `hijacked_observables[name]`, and sets `ax[name].val = false`.
+Thus, even though the rest of the listeners will continue to receive updates from this observable
+(in case there is a need for it to remain), its value will remain `false`.
+"""
+macro hijack_observable(name)
+    return esc(quote
+        getproperty(ax, $name)[] = $(false)
+        hijacked_observables[$name] = Observable($(true))
+        __listener = on(getproperty(ax, $name)) do hijacked_obs_value
+            hijacked_observables[$name][] = hijacked_obs_value
+            getproperty(ax, $name).val = $(false)
+        end
+        # give our listener absolute priority; that means that
+        # our function will be executed first.  Even though the
+        # axis will _receive_ the signal, when it _reads_ the
+        # Observable's value, it will see only `false`.
+        pop!(getproperty(ax, $name).listeners)
+        pushfirst!(getproperty(ax, $name).listeners, __listener.f)
+
+    end)
+
+end
