083daf5e94575500b6300f11d6b579db21e76706
Anshul Singhvi <anshulsinghvi@gmail.com>
2022-04-27 14:28:43 -0400

    copy doc images to the correct directory

--- docs/generate_examples.jl
+++ docs/generate_examples.jl
@@ -15,7 +15,7 @@ example_title_pairs = [
 
 io = open(joinpath("docs", "src", "examples.md"), "w")
 
-assetpath = mkpath(joinpath("docs", "assets"))
+assetpath = mkpath(joinpath("docs", "src", "assets"))
 
 for ext in example_title_pairs
 
@@ -34,7 +34,7 @@ for ext in example_title_pairs
     println(io, readchomp(filepath))
     println(io, "```\n")
     println(io, "```@raw html")
-    println(io, "<img src=\"$(joinpath(assetpath, splitext(example)[1]*".png"))\" alt=\"$title\"></img>") # data:image/png;base64,$(base64data)\
+    println(io, "<img src=\"$(joinpath(".", "assets", splitext(example)[1]*".png"))\" alt=\"$title\"></img>") # data:image/png;base64,$(base64data)\
     println(io, "```\n")
 
 end
