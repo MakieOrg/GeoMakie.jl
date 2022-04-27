106e19b783168f227bdf399268db6f466e1c9b8c
Anshul Singhvi <anshulsinghvi@gmail.com>
2022-04-27 12:17:23 -0400

    Fix orthographic example
    - Regression: autolims not working for orthographic, looks like -180:180 
    captured the 0,0 spot somehow

--- examples/orthographic.jl
+++ examples/orthographic.jl
@@ -3,11 +3,15 @@ using GeoMakie, CairoMakie
 # Limited-domain projections (like orthographic) must have
 # their limits correctly set!
 # If the limits are too large, you may get a blank figure.
+lons = -180:180
+lats = -90:90
+field = [exp(cosd(l)) + 3(y/90) for l in lons, y in lats]
+
 fig = Figure()
 ga = GeoAxis(
     fig[1, 1],
     dest="+proj=ortho",
-    lonlims = Makie.automatic,
+    lonlims = (-90,90),
     coastlines = true,
     title = "Orthographic projection with proper limits"
 )
