4a2ddea04a4f4e2c746b71b14142dee3ca0061b0
Anshul Singhvi <anshulsinghvi@gmail.com>
2022-05-03 22:23:00 -0400

    fix rotation mess

--- examples/rotating_earth.jl
+++ examples/rotating_earth.jl
@@ -13,7 +13,7 @@ image!(-180..180, -90..90, rotr90(GeoMakie.earth()); interpolate = false)
 hidedecorations!(ga)
 
 record(fig, "rotating_earth_ortho.mp4"; framerate=30) do io
-    for lon in -180:3:180
+    for lon in -90:90
         ga.title[] = string(lon) * "°"
         destnode[] = "+proj=ortho +lon_0=$lon"
         xlims!(ga, lon-90, lon+90)
