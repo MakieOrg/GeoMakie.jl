28abbaae04876c4104e78bd4c9f8514c43234ef2
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-08-16 16:36:27 +0200

    Drop note

--- src/geoaxis.jl
+++ src/geoaxis.jl
@@ -121,7 +121,6 @@ function GeoAxis(args...;
     Makie.Observables.connect!(ax.scene.transformation.transform_func, _transformation)
 
     # Plot coastlines
-    # TODO: Fix this! (starting at element 94 of coastlines, things get weird)
     coast_line = GeoMakie.geoJSONtraitParse.(GeoInterface.geometry.(GeoMakie.coastlines()))
     coastplot = lines!(ax, coast_line; color = :black, coastline_attributes...)
     translate!(coastplot, 0, 0, 99) # ensure they are on top of other plotted elements
