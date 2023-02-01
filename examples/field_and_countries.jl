5394631dabcaf817ea4fcf42888b30e749ea0ed7
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-02-01 21:47:14 +0530

    Update example for new GeoAxis

--- examples/field_and_countries.jl
+++ examples/field_and_countries.jl
@@ -8,7 +8,7 @@ using GeometryBasics
 using GeoInterface
 
 # https://datahub.io/core/geo-countries#curl # download data from here
-worldCountries = GeoJSON.read(read(Downloads.download("https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json"), String))
+worldCountries = GeoJSON.read(read(Downloads.download("https://raw.githack.com/johan/world.geo.json/master/countries.geo.json"), String))
 n = length(worldCountries)
 lons = -180:180
 lats = -90:90
@@ -18,9 +18,10 @@ fig = Figure(resolution = (1200,800), fontsize = 22)
 
 ax = GeoAxis(
     fig[1,1];
-    dest = "+proj=wintri",
+    target_projection = "+proj=vandg",
     title = "World Countries",
     tellheight = true,
+    limits = ((-180, 180), (-90, 90))
 )
 
 hm1 = surface!(ax, lons, lats, field; shading = false)
