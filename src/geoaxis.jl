8b1411dba1e53a377af605ddfc94025f50cc248d
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-02-14 16:54:44 +0530

    Move geospine_obs to geoaxis.jl

--- src/geoaxis.jl
+++ src/geoaxis.jl
@@ -787,6 +787,89 @@ function geomakie_transform(trans::Observable, obs...)
 end
 
 
+
+"""
+    geospine_obs(ga::GeoAxis; padding = 10, density = 2)::Observable{Vector{Point2f}}
+
+This function returns an Observable which represents the true border of the geoaxis in question.  
+The observable is lifted on the geoaxis's transform, scene, and final (transformed) limits.
+
+## Approach
+
+Basically, we create a grid of density `density` of the Scene's pixel area, 
+in transformed space (with some `padding` added to each side).  Then, we 
+apply the inverse of `ga.transform_func` to the grid positions, which 
+projects them from transformed to input (lon/lat) space.  This yields
+finite values where the transformed values correspond to some lon/lat,
+and infinite values where they do not.
+
+These values form a Boolean mask.  We find the contour of this mask at level 0.5, 
+then return that as a vector of Point2f in the pixelspace of `ga.blockscene`, to which
+this can be plotted.
+
+## Keyword arguments
+- `padding`: Pads each side by that amount of space in transformed space.  Helps with transforms.
+- `density`: The pixel density of the grid - each real pixel will have `density^2` grid cells.
+
+"""
+function geospine_obs(ga::GeoAxis; padding = 10, density = 2)
+    # how to find the border of a projected area in nonlinearly transformed space:
+    # operate in transformed space, in the Scene's limits
+    # project to input space
+    # whatever is infinite/invalid is then outside lat long space
+    # from this, the border/spine line is just the contour at 0.5, 
+    # if valid → true and invalid → false.
+    spineline = Observable(Point2f[])
+    lift(ga.transform_func, ga.scene.px_area, ga.finallimits) do ptrans, pxarea, finallims
+        # empty the spineline
+        empty!(spineline.val)
+        # TODO: if you zoom in sufficiently, this should somehow correct itself or otherwise not display
+        # but how do you do that?
+        # maybe have some criterion - either geospine or normal spines, but not both
+        # create a grid of the Scene's pixel area, in transformed space
+        # TODO: apply padding in pixelspace, then project down to transformed space.
+        xmin, ymin = minimum(finallims)
+        xmax, ymax = maximum(finallims)
+        xs = Float64.(LinRange(xmin - padding, xmax + padding, round(Int, (pxarea.widths[1] + 2 * padding) * density))) # resolution is 2x pixel resolution, no particular reason.
+        ys = Float64.(LinRange(ymin - padding, ymax + padding, round(Int, (pxarea.widths[2] + 2 * padding) * density)))
+        # points in transformed space
+        tpoints = Point2{Float64}.(xs, ys')
+        # points in lon/lat space
+        itpoints = Makie.apply_transform(Makie.inverse_transform(ptrans), tpoints)
+
+        # possibly, to correct for the padding issue, set every pixel outside the scene
+        # to false, i.e., infinite.  This would make a square each time...
+        finite_mask = isfinite.(itpoints)
+        if padding != 0
+            # bottom
+            finite_mask[1:padding, :] = false
+            # top
+            finite_mask[(end-padding):end, :] = false
+            # left
+            finite_mask[:, 1:padding] = false
+            # right
+            finite_mask[:, (end-padding):end] = false
+        end
+
+        # there are only two possible values in the finite mask.
+        # so, we can compute the correct spine contour.
+        spine_contour = Makie.Contours.contours(xs, ys, Float32.(finite_mask), [0.5f0])
+        # since we know there's only one level, instead of iterating through all levels,
+        # we can skip that and iterate only through the first.
+        for element in Makie.Contours.lines(first(Makie.Contours.levels(spine_contour)))
+            append!(
+                spineline.val, 
+                Point2f.(Makie.project.((camera(ga.scene),), :data, :pixel, element.vertices)) .+ (pxarea.origin,)
+            )
+        end
+        # we've constructed the spine line, now start the update.
+        notify(spineline)
+
+    end
+
+    return spineline
+end
+
 # add docs for GeoAxis
 
 # Base.@doc GeoAxis """
