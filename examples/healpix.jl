42654e6dea545021e428861e7038fe14e9a3c41f
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-06-18 19:32:05 -0400

    Correct some oversights

--- examples/healpix.jl
+++ examples/healpix.jl
@@ -9,6 +9,8 @@ This is currently a manual process, but we hope to add recipes soon!
 
 # Construct a synthetic Healpix map
 
+using CairoMakie, GeoMakie
+
 using Healpix
 nside = 8
 m = HealpixMap{Float64, RingOrder}(nside)
@@ -17,7 +19,7 @@ m
 #
 img, _, _ = Healpix.equirectangular(m)
 heatmap(img)
-# Now we can plot it on a GeoAxis with a mollweide projection:
+# Now we can plot it on a GeoAxis with a Mollweide projection:
 meshimage(-180..180, -90..90, reverse(img; dims = 1); npoints = 200, axis = (; type = GeoAxis, dest = "+proj=moll"))
 # Finally, we can also try to obtain the image as a Mollweide projected image via Healpix, and then plot it directly.
 # For more information on what we're doing here, see the [Multiple CRS](@ref) example.
@@ -26,8 +28,8 @@ heatmap(img)
 # 
 fig = Figure()
 ax = GeoAxis(fig[1, 1]; dest = "+proj=moll")
-hp_bbox = Makie.apply_transform(ax.transform_func[], Makie.BBox(-180, 180, -90, 90))
-#
+hp_bbox = Makie.apply_transform(ax.transform_func[], Makie.BBox(-180, 180, -90, 90)) 
+# The rectangle above is the bounding box of the full Earth (or sky, in this case) in the Mollweide projection.
 mini = minimum(hp_bbox)
 maxi = maximum(hp_bbox)
 meshimage!(ax, mini[1]..maxi[1], mini[2]..maxi[2], reverse(img; dims = 1); npoints = 200, source = "+proj=moll")
