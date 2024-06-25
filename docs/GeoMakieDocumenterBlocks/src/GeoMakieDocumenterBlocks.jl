a4381cb8983d7ba1aed8348a77b4981b6e6c0f96
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-06-25 07:41:59 -0400

    use Makie

--- docs/GeoMakieDocumenterBlocks/src/GeoMakieDocumenterBlocks.jl
+++ docs/GeoMakieDocumenterBlocks/src/GeoMakieDocumenterBlocks.jl
@@ -1,5 +1,10 @@
 module GeoMakieDocumenterBlocks
 
+using Documenter
+using ImageTransformations, ImageIO, Base64, FileIO # for resize
+import Documenter: MarkdownAST
+using Makie
+
 const GALLERY_DICT = Dict{String, Any}()
 
 export GALLERY_DICT
