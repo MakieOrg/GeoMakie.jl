264aef970aa2b9a402eaa5279473a1fdb01bcd51
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-07-13 17:04:32 +0200

    remove strange sphere

--- examples/specialized/earth3d.jl
+++ examples/specialized/earth3d.jl
@@ -2,13 +2,6 @@ using GLMakie, GeoMakie, Geodesy
  
 transf = Geodesy.ECEFfromLLA(Geodesy.WGS84())
 
-transf2 = Makie.PointTrans{3}() do p
-    ϕ, θ, r = p
-    sθ, cθ = sincos(deg2rad(θ))
-    sϕ, cϕ = sincos(deg2rad(ϕ))
-    Point3(r * cθ * cϕ, r * sθ * cϕ, r * sϕ)
-end
-
 f, a, p = meshimage(-180..180, -90..90, GeoMakie.earth(); npoints = 100, z_level = 0, axis = (; type = LScene));
 lp = lines!(a, Point3f.(1:10, 1:10, 110); color = :red, linewidth = 2)
 cc = cameracontrols(a.scene)
