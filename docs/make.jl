63749663c9f6d23846a4b8b2e7479b1567dda133
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-10-12 07:54:11 -0700

    Clean up `makedocs`

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,9 @@
-using Documenter, DocumenterVitepress, Literate
+using Documenter, DocumenterVitepress, Literate, OhMyCards
 using GeoMakie, CairoMakie, Makie, GeoInterfaceMakie
 
 # some strategic imports to avoid world age issues
 using FHist
 
-# include(joinpath(dirname(@__DIR__), "GeoMakieDocumenterBlocks", "src", "GeoMakieDocumenterBlocks.jl"))
-using OhMyCards
 # Set some global settings
 # Good quality CairoMakie with PNG
 CairoMakie.activate!(px_per_unit = 2, type = :png)
@@ -94,59 +92,3 @@ deploydocs(;
     push_preview = true, 
     forcepush = true
 )
-
-
-# publicpath = joinpath(@__DIR__, "build", ".documenter", "public")
-# mkpath(publicpath)
-# mv(joinpath(@__DIR__, "build", ".documenter", "examples", "covers"), joinpath(publicpath, "covers"))
-
-# DocumenterVitepress.build_docs(joinpath(@__DIR__, "build"))
-
-# rm(joinpath(@__DIR__, "build", ".documenter"); recursive = true, force = true)
-# contents = readdir(joinpath(@__DIR__, "build", "final_site"))
-# for item in contents
-#     src = joinpath(joinpath(@__DIR__, "build"), "final_site", item)
-#     dst = joinpath(joinpath(@__DIR__, "build"), item)
-#     cp(src, dst)
-# end
-# rm(joinpath(@__DIR__, "build", "final_site"); recursive = true)
-
-
-# isdir(joinpath(@__DIR__, "src", "examples")) && rm.(readdir(joinpath(@__DIR__, "src", "examples"); join = true); force = true)
-
-        # "Examples" => [
-        #     "Basic examples" => "examples/basic.md",
-        #     "New API" => "examples/new.md",
-        #     "Orthographic projection" => "examples/orthographic.md",
-        #     "German Lakes" => "examples/german_lakes.md",
-        #     "Geostationary satellite image" => "examples/geostationary_image.md",
-        #     "Contourf" => "examples/contourf.md",
-        #     # "Multiple CRS" => "examples/multiple_crs.md",
-        #     "World Population centers" => "examples/world_population.md",
-        #     "Field and countries" => "examples/field_and_countries.md",
-        #     "Mesh image recipe" => "examples/meshimage.md",
-        #     # "Geodetic transformation to the sphere" => "examples/geodesy.md",
-        #     "Axis configuration" => "examples/axis_config.md",
-        #     # "Italy's states" => "examples/italy.md",
-        #     # "Most Projections" => "examples/most_projections.md",
-        #     "Projections" => "examples/projections.md",
-        #     # "GraphMakie with GeoMakie" => "examples/graph_on_usa.md",
-        # ],
-
-    
-# # use Literate for examples
-# examples = readdir(example_dir; join = true)
-# exclude = Set(["geodesy.jl", "makiecon_examples.jl", "multiple_crs.jl"])
-# filter!(examples) do file
-#     isfile(file) && !(basename(file) in exclude) && endswith(file, ".jl")
-# end
-
-
-# for example in examples
-#     Literate.markdown(example, joinpath(@__DIR__, "src", "examples"); documenter = true)
-# end
-
-# documenter_example_paths = joinpath.(
-#     ("examples"), 
-#     first.(splitext.(last.(splitdir.(examples)))) .* (".md",)
-# )
