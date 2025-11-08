3be26df0540bdd2f2c317b404bb394abc8892bb1
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-11-07 21:31:23 -0500

    Set up tests for globe axis

---
+++ test/basics.jl
@@ -0,0 +1,23 @@
+@testset "Basics" begin
+    lons = -180:180
+    lats = -90:90
+    field = [exp(cosd(l)) + 3(y/90) for l in lons, y in lats]
+
+    fig = @test_nowarn Figure()
+    ax = @test_nowarn GeoAxis(fig[1,1])
+    el = @test_nowarn surface!(ax, lons, lats, field; shading = NoShading)
+    @test true
+    # display(fig)
+end
+
+@testset "geo2basic" begin
+    @test GeoMakie.coastlines() isa Vector
+    @test GeoMakie.coastlines()[1] isa GeometryBasics.LineString
+end
+
+@testset "Line Splitting" begin
+    @test split(GeoMakie.coastlines(),"+lon_0=-160") isa Vector
+    ga = @test_nowarn GeoAxis(Figure();dest = "+proj=wintri +lon_0=-160")
+    @test GeoMakie.coastlines(ga) isa Observable
+    @test GeoMakie.coastlines(ga)[] isa AbstractVector
+end
--- test/globeaxis.jl
+++ test/globeaxis.jl
@@ -1,2 +1,30 @@
-fig = Figure()
-ga = GlobeAxis(fig[1,1])
+using Test
+using GeoMakie
+using Makie
+using Geodesy
+@testset "Basic instantiation" begin
+    fig = Figure()
+    ga = @test_nowarn GlobeAxis(fig[1,1])
+    @test_nowarn Makie.update_state_before_display!(ga)
+    @test_nowarn lines!(ga, GeoMakie.coastlines())
+    @test_nowarn surface!(ga, -180..180, -90..90, rand(100, 100); shading = NoShading)
+    @test_nowarn scatter!(ga, 0, 0, 0)
+    @test_nowarn Makie.update_state_before_display!(ga)
+end
+
+@testset "Camera control" begin
+    f, a, p = @test_nowarn meshimage(-180..180, -90..90, GeoMakie.earth(); uv_transform = :rotr90, npoints = 100, z_level = 0, axis = (; type = GlobeAxis))
+    @test_nowarn Makie.update_state_before_display!(a)
+
+    f, a, p = @test_nowarn meshimage(
+        -180..180, -90..90, GeoMakie.earth(); 
+        uv_transform = :rotr90,
+        npoints = 100, z_level = 0, axis = (; type = GlobeAxis, camera_longlat = (0, 0), camera_altitude = 1e7)
+    )
+    @test_nowarn Makie.update_state_before_display!(a)
+    current_eyepos = cameracontrols(a.scene).eyeposition[]
+
+    meshimage!(a, -180..180, -90..90, GeoMakie.earth(); uv_transform = :rotr90, npoints = 100, z_level = 2e8, reset_limits = false, xautolimits = false, yautolimits = false, zautolimits = false)
+    @test_nowarn Makie.update_state_before_display!(a)
+    @test cameracontrols(a.scene).eyeposition[] == current_eyepos
+end
